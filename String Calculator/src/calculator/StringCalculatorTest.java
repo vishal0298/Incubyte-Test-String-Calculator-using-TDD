cbdb66c9107a29791318a41bc85d551396d37713
Vishal Pawar <vishalpawar0298@gmail.com>
2021-10-17 23:09:08 +0530

    TestCase 3 : string Should Handle Multiple Input

--- String Calculator/src/calculator/StringCalculatorTest.java
+++ String Calculator/src/calculator/StringCalculatorTest.java
@@ -18,4 +18,10 @@ class StringCalculatorTest {
 	    void stringWithSingleNumberShouldReturnNumberAsInt() {
 		assertEquals(1, stringCalculator.add("1"));
 	    }
+	
+	@Test
+	 @DisplayName("string Should Handle Multiple Input")
+	    void stringShouldhandleMultipleInput() {
+		assertEquals(45, stringCalculator.add("1,2,3,4,5,6,7,8,9"));
+	    }
 }
