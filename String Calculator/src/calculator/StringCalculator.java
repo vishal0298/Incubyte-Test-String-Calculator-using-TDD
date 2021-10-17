c70a37944bb85fb2534e4fcfed90dc163b427cab
Vishal Pawar <vishalpawar0298@gmail.com>
2021-10-17 20:50:15 +0530

    first commit

---
+++ String Calculator/src/calculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class StringCalculator {
+	
+	public int add(String numbers)  {
+        return 0;
+    }
+}
---
+++ String Calculator/src/calculator/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringCalculatorTest {
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
